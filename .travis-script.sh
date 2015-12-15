ec109ee08c5be000ad8f2651c88af49733ee9d4e
Graham Taylor <gtayzlor@gmail.com>
2015-12-15 19:29:42 +0000

    Re-add dist to gitignore, update script to only do docker login if we are doing docker tasks

--- .travis-script.sh
+++ .travis-script.sh
@@ -4,12 +4,14 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   # Handle master builds
   echo "Building Docker image for ${TRAVIS_BRANCH}"
   grunt build
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" tutum.co
   docker build -t tutum.co/$DOCKER_USERNAME/angular-cart-demo:latest .
   docker push tutum.co/$DOCKER_USERNAME/angular-cart-demo:latest
 elif [ -n "$TRAVIS_TAG" ]; then
   # Handle tagged builds
   echo "Building Docker image for ${TRAVIS_TAG}"
   grunt build
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" tutum.co
   docker build -t tutum.co/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG .
   docker push tutum.co/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG
 else
