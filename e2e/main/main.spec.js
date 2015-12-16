eb04769660b8f9acc93287c2976881a6f419a4e8
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2015-12-16 12:56:31 +0000

    CodeClimate : 'page' is defined but never used

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -6,10 +6,12 @@ describe('Main View', function() {
   var page;
 
   beforeEach(function() {
+
     browser.get(config.baseUrl + '/');
   });
 
   it('should have a title', function() {
+    
     expect(browser.getTitle()).toEqual('Angular Cart Demo');
   });
 });
