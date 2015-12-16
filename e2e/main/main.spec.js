79aac50006814d1204605e495426020d849f5bd8
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2015-12-16 12:47:17 +0000

    Updated protractor main.spec

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -10,9 +10,7 @@ describe('Main View', function() {
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Angular Cart Demo');
   });
 });
