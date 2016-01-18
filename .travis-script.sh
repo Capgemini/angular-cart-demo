12b37b40bfcbe9cfcf42ab0d5ab6d4bd053f1caf
Cam Parry <cam.parry@gmail.com>
2016-01-18 17:43:33 +0000

    Add some docker tests to travis script
    fix getting pip
    
    fix getting pip
    
    fix getting pip
    
    upgrade pip first
    
    docker squash image correctly

--- .travis-script.sh
+++ .travis-script.sh
@@ -4,16 +4,18 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
   # Handle master builds
   echo "Building Docker image for ${TRAVIS_BRANCH}"
   grunt build
-  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" tutum.co
-  docker build -t tutum.co/$DOCKER_USERNAME/angular-cart-demo:latest .
-  docker push tutum.co/$DOCKER_USERNAME/angular-cart-demo:latest
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+  docker build -t $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:latest .
+  docker-scripts squash $DOCKER_USERNAME/angular-cart-demo:latest
+  docker push $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:latest
 elif [ -n "$TRAVIS_TAG" ]; then
   # Handle tagged builds
   echo "Building Docker image for ${TRAVIS_TAG}"
   grunt build
-  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" tutum.co
-  docker build -t tutum.co/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG .
-  docker push tutum.co/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+  docker build -t $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG .
+  docker-scripts squash $DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG
+  docker push $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG
 else
   # unit + integration tests
   npm test
@@ -26,4 +28,10 @@ else
   npm run coverage-report
   codeclimate-test-reporter < coverage/server/unit/lcov.info
   codeclimate-test-reporter < coverage/server/integration/lcov.info
+
+  # Docker tests
+  docker run --rm -v $(pwd):/lint lukasmartinelli/hadolint hadolint /lint/Dockerfile
+  docker run -v $(pwd):/app -v $(pwd)/lynis-logs:/var/log dduportal/lynis:2.1.0 \
+    --auditor "Automator" --quick audit dockerfile /app/Dockerfile
+
 fi
