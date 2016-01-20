e4c8eacfc8ae375239d0ab66fec713edddd68cc0
Cam Parry <cam.parry@gmail.com>
2016-01-20 13:59:01 +0000

    Fix travis docker scripts
    fix codeclimate issue

--- .travis-script.sh
+++ .travis-script.sh
@@ -6,7 +6,6 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
   grunt build
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
   docker build -t $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:latest .
-  docker-scripts squash $DOCKER_USERNAME/angular-cart-demo:latest
   docker push $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:latest
 elif [ -n "$TRAVIS_TAG" ]; then
   # Handle tagged builds
@@ -14,7 +13,6 @@ elif [ -n "$TRAVIS_TAG" ]; then
   grunt build
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
   docker build -t $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG .
-  docker-scripts squash $DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG
   docker push $DOCKER_REGISTRY/$DOCKER_USERNAME/angular-cart-demo:$TRAVIS_TAG
 else
   # unit + integration tests
@@ -36,7 +34,7 @@ else
   docker run -v $(pwd):/app -v $(pwd)/lynis-logs:/var/log dduportal/lynis:2.1.0 \
     --auditor "Automator" --quick audit dockerfile /app/Dockerfile
 
-  grunt serve
+  nohup grunt serve &
   docker run -u zap -i owasp/zap2docker-stable zap-cli quick-scan -sc \
     -o '-config api.disablekey=true' -s xss,sqli --spider "http://localhost:9000"
 
